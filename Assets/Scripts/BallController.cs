fc507076ecb187db677fafd8ddd6eff0bfcc9d0c
sol9342 <solanag44@gmail.com>
2025-02-05 13:07:54 -0800

    Basic bowling and setup
    Ball, plane and simple cylinder prefab pins

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class BallController : MonoBehaviour
+{
+    [SerializeField] private float force = 1f;
+    [SerializeField] private InputManager inputManager;
+
+    private Rigidbody ballRB;
+
+    private bool isBallLaunched;
+
+    
+    void Start()
+    {
+        // getting reference to rigid body
+        ballRB = GetComponent<Rigidbody>();
+        
+        // add a listener to OnSpacePressed Events
+        // when space key is pressed, the LaunchBall method will be called
+        inputManager.OnSpacePressed.AddListener(LaunchBall);
+    }
+
+    private void LaunchBall()
+    {
+        // if the ball is launched, don't do anything
+        if(isBallLaunched) return;
+        // when ball is not launched, set it to true and launch ball
+        isBallLaunched = true;
+        ballRB.AddForce(transform.forward * force, ForceMode.Impulse);
+    }
+}
