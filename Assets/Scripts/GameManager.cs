ec48e16c58170f220c737a9fff0b5acd55844ea6
sol9342 <solanag44@gmail.com>
2025-02-06 21:07:16 -0800

    Added scoring UI

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,8 +1,10 @@
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private float score = 0;
+    [SerializeField] private TextMeshProUGUI scoreText;
     private FallTrigger[] pins;
 
     private void Start()
@@ -19,6 +21,7 @@ public class GameManager : MonoBehaviour
     private void IncrementScore()
     {
         score++;
+        scoreText.text = $"Score: {score}";
     }
 
 }
