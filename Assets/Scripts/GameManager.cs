50a91ddc731ae825a83eaf4b86568d2143252afb
sol9342 <solanag44@gmail.com>
2025-02-06 20:50:28 -0800

    Pin counter script added
    In GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private float score = 0;
+    private FallTrigger[] pins;
+
+    private void Start()
+    {
+        // find all objects of type FallTrigger
+        pins = FindObjectsByType<FallTrigger>(FindObjectsInactive.Include, FindObjectsSortMode.None);
+
+        foreach(FallTrigger pin in pins)
+        {
+            pin.OnPinFall.AddListener(IncrementScore);
+        }
+    }
+
+    private void IncrementScore()
+    {
+        score++;
+    }
+
+}
